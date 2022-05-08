7ba8b300610a406f77ee351e01b6248f85a39498
DrewnowskiBartlomiej <20323@ecam.be>
2022-05-08 20:20:50 +0200

    update game_edit component

--- Frontend/biblio_jeux_ang/src/app/game-edit/game-edit.component.ts
+++ Frontend/biblio_jeux_ang/src/app/game-edit/game-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService, Game } from '../rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-edit',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameEditComponent implements OnInit {
 
-  constructor() { }
+  game = {} as Game;
+
+  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.rest.getGame(this.route.snapshot.params['game_id']).subscribe(
+      (data)=> {
+        console.log(data);
+        this.game = data;
+      }
+    )
   }
 
+  updateGame() {
+    this.rest.updateGame(this.game).subscribe(
+      (result) =>{
+        this.router.navigate(['/games']);
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
+
+  cancelEdit() {
+    this.router.navigate(['/games']);
+  }
 }
