19217dc33b73fdda0978e9047d5b93f4e863406d
DrewnowskiBartlomiej <20323@ecam.be>
2022-05-08 19:57:57 +0200

    Creat rest services
    init methods of games, categories and platforms

--- Frontend/biblio_jeux_ang/src/app/app.module.ts
+++ Frontend/biblio_jeux_ang/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { GameEditComponent } from './game-edit/game-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    GameEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/biblio_jeux_ang/src/app/rest.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+
+const endpoint = "http://localhost:8000/";
+
+//Interface game
+export interface Game{
+  game_id: number;
+  name: string;
+  description: string;
+  cost: number;
+  memory: number;
+}
+
+//Interface category
+export interface Category{
+  category_id: number;
+  name: string;
+  description: string;
+}
+
+//Interface platforms
+export interface Platform{
+  platform_id: number;
+  name: string;
+  description: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { }
+
+
+  // http methods of games
+  getGames(): Observable<Game[]> {
+    return this.http.get<Game[]>(endpoint + 'games');
+  }
+  CreateGame(game:Game): Observable<any> {
+    console.log(game);
+    return this.http.post<Game>(endpoint + 'game', game);
+  }
+  updateGame(game:Game): Observable<any> {
+    return this.http.put<Game>(endpoint + 'game/' + game.game_id, game);
+  }
+  deleteGame(id: number): Observable<any> {
+    return this.http.delete<Game>(endpoint + 'game/' + id);
+  }
+  getGame(id: number): Observable<any> {
+    return this.http.get<Game>(endpoint + "game/find/"+ id);
+  }
+  
+  // http methods of categories
+  getCategorys(): Observable<Category[]> {
+    return this.http.get<Category[]>(endpoint + 'categories');
+  }
+  CreateCategory(category:Category): Observable<any> {
+    console.log(category);
+    return this.http.post<Category>(endpoint + 'category', category);
+  }
+  updateCategory(category:Category): Observable<any> {
+    return this.http.put<Category>(endpoint + 'category/' + category.category_id, category);
+  }
+  deleteCategory(id: number): Observable<any> {
+    return this.http.delete<Category>(endpoint + 'category/' + id);
+  }
+  getCategory(id: number): Observable<any> {
+    return this.http.get<Category>(endpoint + "category/find/"+ id);
+  }
+
+  // http methods of platforms
+  getPlatforms(): Observable<Platform[]> {
+    return this.http.get<Platform[]>(endpoint + 'platforms');
+  }
+  CreatePlatform(platform:Platform): Observable<any> {
+    console.log(platform);
+    return this.http.post<Platform>(endpoint + 'platform', platform);
+  }
+  updatePlatform(platform:Platform): Observable<any> {
+    return this.http.put<Platform>(endpoint + 'platform/' + platform.platform_id, platform);
+  }
+  deletePlatform(id: number): Observable<any> {
+    return this.http.delete<Platform>(endpoint + 'platform/' + id);
+  }
+  getPlatform(id: number): Observable<any> {
+    return this.http.get<Platform>(endpoint + "platform/find/"+ id);
+  }
+  
+}
