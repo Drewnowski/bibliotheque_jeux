68f46475ecde9d4707f93c1c19ef71975b40bd83
DrewnowskiBartlomiej <20323@ecam.be>
2022-05-08 20:34:35 +0200

    update games componet

--- Frontend/biblio_jeux_ang/src/app/games/games.component.ts
+++ Frontend/biblio_jeux_ang/src/app/games/games.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Game, RestService } from '../rest.service';
 
 @Component({
   selector: 'app-games',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesComponent implements OnInit {
 
-  constructor() { }
+  game = {} as Game;
+  games: Game[] = [];
+
+  constructor(public rest: RestService,private route: ActivatedRoute ,private router: Router) { }
 
   ngOnInit(): void {
+    this.getGames();
   }
 
+  getGames(){
+    this.rest.getGames().subscribe(
+      resp => (
+        console.log(resp),
+        this.games = resp
+    )
+    );
+  }
+  add(){
+    this.router.navigate(['/game_add']);
+  }
+
+  deleteGame(id: number){
+    this.rest.deleteGame(id).subscribe();
+    this.getGames();
+    //this.router.navigate(['/destinations']);
+  }
 }
