a8f11b5fc112c709d0e99b71bedae38f6460e5e0
DrewnowskiBartlomiej <20323@ecam.be>
2022-05-08 20:08:18 +0200

    Update app-routing.module.ts
    add routes

--- Frontend/biblio_jeux_ang/src/app/app-routing.module.ts
+++ Frontend/biblio_jeux_ang/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameEditComponent } from './game-edit/game-edit.component';
+import { GamesComponent } from './games/games.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'games',
+    component: GamesComponent
+  },
+  {
+    path: 'game-edit/:games_id',
+    component: GameEditComponent
+  },
+  {
+    path: '',
+    redirectTo: 'games',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
